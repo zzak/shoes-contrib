e4525056194f2d708377d5c4a2c1cb874543ef72
Steve Klabnik <steve@steveklabnik.com>
2010-11-01 08:56:46 -0400

    Retabbing search.rb.

--- basic/search.rb
+++ basic/search.rb
@@ -1,18 +1,18 @@
 Shoes.app :title => "Search sample", :width => 300, :height => 400 do
-	msg = "Enter a search."
-	stack :margin => 5 do
-		@search = edit_line :width => 280
+  msg = "Enter a search."
+  stack :margin => 5 do
+    @search = edit_line :width => 280
 
-		@search.change do |search|
-			if search.text.empty?
-				@results.clear{ para msg }
-			else
-				@results.clear { do_search(search.text) }
-			end 
-		end
+    @search.change do |search|
+      if search.text.empty?
+        @results.clear{ para msg }
+      else
+        @results.clear { do_search(search.text) }
+      end 
+    end
 
-		@results = flow{ para msg }
-	end
+    @results = flow{ para msg }
+  end
 end
 
 def do_search(word)
@@ -20,13 +20,13 @@ def do_search(word)
   data = %w{ place plan plant plot face race rails ruby train trouble double }
   
   data.each do |entry|
-		if /#{word}/ =~ entry
-			@results.append do 
-				stack :margin_top => 5 do 
-					background lightyellow, :curve => 6
-					para entry
-				end
-			end
-		end					
-	end
+    if /#{word}/ =~ entry
+      @results.append do 
+        stack :margin_top => 5 do 
+          background lightyellow, :curve => 6
+          para entry
+        end
+      end
+    end         
+  end
 end
